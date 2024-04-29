fd1e4fea53dbdb8186bd6635c5e4153115d8fdd5
fan <abrclano@gmail.com>
2024-04-29 19:29:08 +0800

    feat: add my nvim config

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # yourNvim
+> 基于[NvChad2.0](https://github.com/NvChad/NvChad/tree/v2.0)的自用nvim配置
---
+++ lua/custom/configs/overrides.lua
@@ -0,0 +1,105 @@
+local M = {}
+
+M.treesitter = {
+  ensure_installed = {
+    "vim",
+    "lua",
+    "html",
+    "css",
+    "javascript",
+    "typescript",
+    "tsx",
+    "c",
+    "markdown",
+    "markdown_inline",
+  },
+  indent = {
+    enable = true,
+    -- disable = {
+    --   "python"
+    -- },
+  },
+}
+
+M.mason = {
+  ensure_installed = {
+    -- lua stuff
+    "lua-language-server",
+    "stylua",
+
+    -- web dev stuff
+    "css-lsp",
+    "html-lsp",
+    "typescript-language-server",
+    "deno",
+    "prettier",
+
+    -- c/cpp stuff
+    "clangd",
+    "clang-format",
+  },
+}
+
+-- git support in nvimtree
+M.nvimtree = {
+  sort_by = "case_sensitive",
+  hijack_cursor = true,
+  system_open = {
+    cmd = "open",
+  },
+  git = {
+    enable = true,
+  },
+  view = {
+    width = 7,
+    adaptive_size = true,
+  },
+  update_focused_file = {
+    enable = true,
+  },
+  renderer = {
+    group_empty = true,
+    icons = {
+      show = {
+        git = true,
+        file = true,
+        folder = true,
+        folder_arrow = true,
+      },
+      glyphs = {
+        default = "",
+        symlink = "",
+        bookmark = "󰆤",
+        modified = "●",
+        folder = {
+          arrow_closed = "",
+          arrow_open = "",
+          default = "",
+          open = "",
+          empty = "",
+          empty_open = "",
+          symlink = "",
+          symlink_open = "",
+        },
+        git = {
+          unstaged = "✗",
+          staged = "✓",
+          unmerged = "⌥",
+          renamed = "➜",
+          untracked = "★",
+          deleted = "⊖",
+          ignored = "◌",
+        },
+      },
+    },
+  },
+  hijack_directories = {
+    enable = true,
+    auto_open = true,
+  },
+  filters = {
+    dotfiles = true,
+  },
+}
+
+return M
